f18f3bd1f51652dc1f4a5c3a15ac230c1fac7264
Utkarsh Misra <63697282+umisra16@users.noreply.github.com>
2020-06-13 20:55:12 -0400

    Create Calculator.py

---
+++ PHY 2048 (Dr. R. Criss)/Calculator.py
@@ -0,0 +1,7 @@
+a = (int(input("Enter homework & participation grade "))/100)*10
+b = (int(input("Enter lowest resular exam "))/100)*10
+c = (int(input("Enter heighest regular exam grade "))/100)*25
+d = (int(input("Enter second heighest regular exam grade "))/100)*25
+e = (int(input("Enter final exam grade "))/100)*30
+f = a + b + c + d + e
+print("Your final grade percentage is ", f,"/ 100")
